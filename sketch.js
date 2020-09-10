36a64b1d46df5738094f52cf37d5a3937d74faaa
SubhojitGhosh-123456789 <subhojit0669@gmail.com>
2020-09-10 17:38:08 +0530

    New

--- sketch.js
+++ sketch.js
@@ -64,7 +64,9 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();   
+    
+
 }
 
 function mouseDragged(){
@@ -74,4 +76,10 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
